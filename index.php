a59a2d60e39a9ed1b27c1804a6e3c01ef91fbcb1
soulyvan <patrickekogha@gmail.com>
2023-05-27 02:31:37 +0000

    Projet PHP de gestion des emplois du temps

--- index.php
+++ index.php
@@ -1,10 +1,40 @@
 <?php
 class Cours
 {
+    private $id;
     private $heure; // pour l'heure
     private $matieres; // un tableau pour les matières de la semaines
 
+    // Méthode pour créer la base de données et la table si elles n'existent pas
+    public static function creerBaseDeDonnees()
+    {
+        $pdo = new PDO('mysql:host=localhost', 'root', '');
+
+        // Création de la base de données si elle n'existe pas
+        $pdo->exec("CREATE DATABASE IF NOT EXISTS projet_ig3");
+
+        // Sélection de la base de données
+        $pdo->exec("USE projet_ig3");
+
+        // Création de la table "cours" si elle n'existe pas
+        $pdo->exec("CREATE TABLE IF NOT EXISTS cours (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            heure VARCHAR(100),
+            courLundi VARCHAR(100),
+            courMardi VARCHAR(100),
+            courMercredi VARCHAR(100),
+            courJeudi VARCHAR(100),
+            courVendredi VARCHAR(100),
+            courSamedi VARCHAR(100)
+        )");
+    }
+
     // Création des Setteurs (nous n'avons pas utiliser les Getteurs ici)
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
     public function setHeure($heure)
     {
         $this->heure = $heure;
@@ -16,20 +46,24 @@ class Cours
     }
 
     // Méthode pour enregistrer une ligne, une ligne contient l'horaire et les matières de la semaine
-    public function enregistrer($id)
+    public function enregistrer()
     {
+        self::creerBaseDeDonnees(); // Vérifier et créer la base de données si nécessaire
+
         $pdo = new PDO('mysql:host=localhost;dbname=projet_ig3', 'root', '');
         $stmt = $pdo->prepare('UPDATE cours SET heure = ?, courLundi = ?, courMardi = ?, courMercredi = ?, courJeudi = ?, courVendredi = ?, courSamedi = ? WHERE id = ?');
 
         $stmt->execute([
             $this->heure, $this->matieres[0], $this->matieres[1], $this->matieres[2],
-            $this->matieres[3], $this->matieres[4], $this->matieres[5], $id
+            $this->matieres[3], $this->matieres[4], $this->matieres[5], $this->id
         ]);
     }
 
     // Méthode pour tout récupérer dans la BDD
     public function recupererCours()
     {
+        self::creerBaseDeDonnees(); // Vérifier et créer la base de données si nécessaire
+
         $pdo = new PDO('mysql:host=localhost;dbname=projet_ig3', 'root', '');
         $stmt = $pdo->query('SELECT * FROM cours');
         $resultats = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -40,6 +74,8 @@ class Cours
     // Méthode pour ajouter une nouvelle ligne (vide) en BDD
     public function ajouter()
     {
+        self::creerBaseDeDonnees(); // Vérifier et créer la base de données si nécessaire
+
         $pdo = new PDO('mysql:host=localhost;dbname=projet_ig3', 'root', '');
         $stmt = $pdo->prepare('INSERT INTO cours (heure, courLundi, courMardi, courMercredi, courJeudi, courVendredi, courSamedi) 
                                VALUES (?, ?, ?, ?, ?, ?, ?)');
@@ -51,6 +87,8 @@ class Cours
     // On supprime entièrement une ligne, à savoir l'heure et tous les cours de la semaine
     public function supprimerLigne($id)
     {
+        self::creerBaseDeDonnees(); // Vérifier et créer la base de données si nécessaire
+
         $pdo = new PDO('mysql:host=localhost;dbname=projet_ig3', 'root', '');
         $stmt = $pdo->prepare('DELETE FROM cours WHERE id = ?');
         $stmt->execute([$id]);
@@ -148,15 +186,16 @@ class Cours
                     // A l'aide d'une boucle, on affiche tous ces enregistrements dans ce format
                     foreach ($resultats as $resultat) {
                         echo "<tr class='entrerValeur'>";
-                        echo "<td class='p-0'><input readonly name='heure' value=\"" . $resultat['heure'] . "\" type='text' class='form-control bg-white rounded-0'></td>";
-                        echo "<td class='p-0'><input readonly name='courDuLundi' value=\"" . $resultat['courLundi'] . "\" type='text' class='form-control bg-white rounded-0'></td>";
-                        echo "<td class='p-0'><input readonly name='courDuMardi' value=\"" . $resultat['courMardi'] . "\" type='text' class='form-control bg-white rounded-0'></td>";
-                        echo "<td class='p-0'><input readonly name='courDuMercredi' value=\"" . $resultat['courMercredi'] . "\" type='text' class='form-control bg-white rounded-0'></td>";
-                        echo "<td class='p-0'><input readonly name='courDuJeudi' value=\"" . $resultat['courJeudi'] . "\" type='text' class='form-control bg-white rounded-0'></td>";
-                        echo "<td class='p-0'><input readonly name='courDuVendredi' value=\"" . $resultat['courVendredi'] . "\" type='text' class='form-control bg-white rounded-0'></td>";
-                        echo "<td class='p-0'><input readonly name='courDuSamedi' value=\"" . $resultat['courSamedi'] . "\" type='text' class='form-control bg-white rounded-0'></td>";
+                        echo "<td class='p-0'><input readonly name='heure[]' value=\"" . $resultat['heure'] . "\" type='text' class='form-control bg-white rounded-0'></td>";
+                        echo "<td class='p-0'><input readonly name='courDuLundi[]' value=\"" . $resultat['courLundi'] . "\" type='text' class='form-control bg-white rounded-0'></td>";
+                        echo "<td class='p-0'><input readonly name='courDuMardi[]' value=\"" . $resultat['courMardi'] . "\" type='text' class='form-control bg-white rounded-0'></td>";
+                        echo "<td class='p-0'><input readonly name='courDuMercredi[]' value=\"" . $resultat['courMercredi'] . "\" type='text' class='form-control bg-white rounded-0'></td>";
+                        echo "<td class='p-0'><input readonly name='courDuJeudi[]' value=\"" . $resultat['courJeudi'] . "\" type='text' class='form-control bg-white rounded-0'></td>";
+                        echo "<td class='p-0'><input readonly name='courDuVendredi[]' value=\"" . $resultat['courVendredi'] . "\" type='text' class='form-control bg-white rounded-0'></td>";
+                        echo "<td class='p-0'><input readonly name='courDuSamedi[]' value=\"" . $resultat['courSamedi'] . "\" type='text' class='form-control bg-white rounded-0'></td>";
                         echo "<td class='p-0 d-none afficheAdmin'><button title='supprimer la ligne' class='btn btn-outline-danger' value='" . $resultat['id'] . "' name='supprimer'><i class='bi bi-x-lg'></i></button></td>";
                         echo "<td class='p-0 d-none afficheAdmin'><button title='enregistrer la ligne' class='btn btn-outline-success' value='" . $resultat['id'] . "' name='modifier'><i class='bi bi-save2'></i></button></td>";
+                        echo "<input class='d-none' type='hidden' name='id[]' value='" . $resultat['id'] . "'>";
                         echo "</tr>";
                     }
 
@@ -169,21 +208,23 @@ class Cours
 
                     // Pour modifier la ligne
                     if (isset($_POST['modifier'])) {
-                        $id = $_POST['modifier'];
+                        $ids = $_POST['id'];
+                        $heures = $_POST['heure'];
+                        $courDuLundi = $_POST['courDuLundi'];
+                        $courDuMardi = $_POST['courDuMardi'];
+                        $courDuMercredi = $_POST['courDuMercredi'];
+                        $courDuJeudi = $_POST['courDuJeudi'];
+                        $courDuVendredi = $_POST['courDuVendredi'];
+                        $courDuSamedi = $_POST['courDuSamedi'];
 
-                        $heure = htmlspecialchars($_POST['heure'], ENT_QUOTES, 'UTF-8');
-                        $matieres;
-                        $matieres[0] = htmlspecialchars($_POST['courDuLundi'], ENT_QUOTES, 'UTF-8');
-                        $matieres[1] = htmlspecialchars($_POST['courDuMardi'], ENT_QUOTES, 'UTF-8');
-                        $matieres[2] = htmlspecialchars($_POST['courDuMercredi'], ENT_QUOTES, 'UTF-8');
-                        $matieres[3] = htmlspecialchars($_POST['courDuJeudi'], ENT_QUOTES, 'UTF-8');
-                        $matieres[4] = htmlspecialchars($_POST['courDuVendredi'], ENT_QUOTES, 'UTF-8');
-                        $matieres[5] = htmlspecialchars($_POST['courDuSamedi'], ENT_QUOTES, 'UTF-8');
+                        foreach ($ids as $key => $id) {
+                            $cours = new Cours();
+                            $cours->setId($id);
+                            $cours->setHeure($heures[$key]);
+                            $cours->setMatiere([$courDuLundi[$key], $courDuMardi[$key], $courDuMercredi[$key], $courDuJeudi[$key], $courDuVendredi[$key], $courDuSamedi[$key]]);
 
-                        $cours->setHeure($heure);
-                        $cours->setMatiere($matieres);
-
-                        $cours->enregistrer($id);
+                            $cours->enregistrer();
+                        }
                     }
                 } else {
                     echo "<div class='alert alert-danger' role='alert'>Il n'y a aucun cours enregistré pour l'instant</div>";
